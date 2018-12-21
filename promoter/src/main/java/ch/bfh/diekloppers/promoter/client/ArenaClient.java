c75f57074a45f6bee76b23041e1579f794ae8bf2
Lukas Hofer <lukas@sportident.com>
2018-12-21 10:41:57 +0100

    Hystix fallback working

--- promoter/src/main/java/ch/bfh/diekloppers/promoter/client/ArenaClient.java
+++ promoter/src/main/java/ch/bfh/diekloppers/promoter/client/ArenaClient.java
@@ -1,12 +1,18 @@
 package ch.bfh.diekloppers.promoter.client;
 
+
 import ch.bfh.diekloppers.promoter.model.Party;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
+@FeignClient(value = "arena-service", fallback = ArenaClientFallback.class)
 public interface ArenaClient {
 
-    String battle(List<Party> challangers);
+    @PostMapping("/battle")
+    String battle(@RequestBody List<Party> challangers);
 
 }
 
--- promoter/src/main/java/ch/bfh/diekloppers/promoter/client/CampClient.java
+++ promoter/src/main/java/ch/bfh/diekloppers/promoter/client/CampClient.java
@@ -1,10 +1,16 @@
 package ch.bfh.diekloppers.promoter.client;
 
 import ch.bfh.diekloppers.promoter.model.Party;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.hateoas.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+
+@FeignClient("camp-service")
 public interface CampClient {
 
-    Resource<Party> createParty(String name);
+    @GetMapping("/createParty")
+    Resource<Party> createParty(@RequestParam(value = "name") String name);
 
 }
