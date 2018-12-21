757a4c492d4a8a577924bc65ae75d67b5853a81f
Lukas Hofer <lukas@sportident.com>
2018-12-21 09:20:48 +0100

    Register services to eureka registry

--- camp/src/main/java/ch/bfh/diekloppers/camp/CampApplication.java
+++ camp/src/main/java/ch/bfh/diekloppers/camp/CampApplication.java
@@ -2,7 +2,9 @@ package ch.bfh.diekloppers.camp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CampApplication {
 
